eb5a4112dab2477d8fa9fef2d5d8d947d82e1128
Vhalen_G <93720406+VhalennnG@users.noreply.github.com>
2023-05-28 05:06:55 +0800

    Add files via upload

---
+++ build/server/pages/_app.js
@@ -0,0 +1,348 @@
+(() => {
+var exports = {};
+exports.id = 888;
+exports.ids = [888];
+exports.modules = {
+
+/***/ 74:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "default": () => (/* binding */ App)
+});
+
+// EXTERNAL MODULE: external "react/jsx-runtime"
+var jsx_runtime_ = __webpack_require__(997);
+// EXTERNAL MODULE: ./node_modules/next/image.js
+var next_image = __webpack_require__(675);
+var image_default = /*#__PURE__*/__webpack_require__.n(next_image);
+// EXTERNAL MODULE: external "react"
+var external_react_ = __webpack_require__(689);
+;// CONCATENATED MODULE: ./components/Footer.js
+
+
+
+function Footer() {
+    return /*#__PURE__*/ (0,jsx_runtime_.jsxs)("footer", {
+        className: "text-gray-600 body-font bg-slate-50",
+        children: [
+            /*#__PURE__*/ (0,jsx_runtime_.jsxs)("div", {
+                className: "container md:px-5 py-10 mx-auto flex md:items-start md:flex-row md:flex-nowrap flex-wrap flex-col w-5/6 justify-between",
+                children: [
+                    /*#__PURE__*/ (0,jsx_runtime_.jsxs)("div", {
+                        className: "md:w-64 flex-shrink-0 text-center md:text-left",
+                        children: [
+                            /*#__PURE__*/ (0,jsx_runtime_.jsxs)("div", {
+                                className: "flex title-font font-medium items-center md:justify-start justify-center text-gray-900 mb-4",
+                                children: [
+                                    /*#__PURE__*/ jsx_runtime_.jsx((image_default()), {
+                                        className: "w-12",
+                                        src: "/logo.png",
+                                        alt: "satria bimbel",
+                                        width: 100,
+                                        height: 100,
+                                        priority: true
+                                    }),
+                                    /*#__PURE__*/ (0,jsx_runtime_.jsxs)("span", {
+                                        className: "ml-3 text-xl font-bold",
+                                        children: [
+                                            "Satria ",
+                                            /*#__PURE__*/ jsx_runtime_.jsx("span", {
+                                                className: "text-blue-300",
+                                                children: "Bimbel"
+                                            })
+                                        ]
+                                    })
+                                ]
+                            }),
+                            /*#__PURE__*/ jsx_runtime_.jsx("p", {
+                                className: "mt-2 text-sm text-gray-500",
+                                children: "Raih prestasimu di sekolah dengan Satria Bimbel"
+                            })
+                        ]
+                    }),
+                    /*#__PURE__*/ jsx_runtime_.jsx("div", {
+                        className: " flex flex-wrap -mb-10 md:mt-0 mt-10 md:text-left text-center",
+                        children: /*#__PURE__*/ (0,jsx_runtime_.jsxs)("div", {
+                            className: "w-full px-4",
+                            children: [
+                                /*#__PURE__*/ jsx_runtime_.jsx("h2", {
+                                    className: "title-font text-gray-900 tracking-widest text-base font-bold mb-3",
+                                    children: "Office"
+                                }),
+                                /*#__PURE__*/ jsx_runtime_.jsx("nav", {
+                                    className: "list-none mb-10",
+                                    children: /*#__PURE__*/ jsx_runtime_.jsx("li", {
+                                        children: /*#__PURE__*/ jsx_runtime_.jsx("a", {
+                                            className: "text-gray-600 hover:text-gray-800",
+                                            children: "Jl. H. Agussalim No. 31 A Kota Parepare"
+                                        })
+                                    })
+                                })
+                            ]
+                        })
+                    })
+                ]
+            }),
+            /*#__PURE__*/ jsx_runtime_.jsx("div", {
+                className: "bg-gray-100",
+                children: /*#__PURE__*/ (0,jsx_runtime_.jsxs)("div", {
+                    className: "container mx-auto py-2 px-5 flex flex-wrap flex-col sm:flex-row w-5/6",
+                    children: [
+                        /*#__PURE__*/ jsx_runtime_.jsx("p", {
+                            className: "text-gray-500 text-sm text-center sm:text-left",
+                            children: "\xa9 2023 Satria Bimbel"
+                        }),
+                        /*#__PURE__*/ (0,jsx_runtime_.jsxs)("span", {
+                            className: "inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-end",
+                            children: [
+                                /*#__PURE__*/ jsx_runtime_.jsx("a", {
+                                    href: "https://www.facebook.com/profile.php?id=100092695375850",
+                                    target: "_blank",
+                                    className: "text-gray-500",
+                                    children: /*#__PURE__*/ jsx_runtime_.jsx("svg", {
+                                        fill: "currentColor",
+                                        strokeLinecap: "round",
+                                        strokeLinejoin: "round",
+                                        strokeWidth: "2",
+                                        className: "w-5 h-5",
+                                        viewBox: "0 0 24 24",
+                                        children: /*#__PURE__*/ jsx_runtime_.jsx("path", {
+                                            d: "M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"
+                                        })
+                                    })
+                                }),
+                                /*#__PURE__*/ jsx_runtime_.jsx("a", {
+                                    href: "https://instagram.com/satriabimbel2023?igshid=NTc4MTIwNjQ2YQ==",
+                                    target: "_blank",
+                                    className: "ml-3 text-gray-500",
+                                    children: /*#__PURE__*/ (0,jsx_runtime_.jsxs)("svg", {
+                                        fill: "none",
+                                        stroke: "currentColor",
+                                        strokeLinecap: "round",
+                                        strokeLinejoin: "round",
+                                        strokeWidth: "2",
+                                        className: "w-5 h-5",
+                                        viewBox: "0 0 24 24",
+                                        children: [
+                                            /*#__PURE__*/ jsx_runtime_.jsx("rect", {
+                                                width: "20",
+                                                height: "20",
+                                                x: "2",
+                                                y: "2",
+                                                rx: "5",
+                                                ry: "5"
+                                            }),
+                                            /*#__PURE__*/ jsx_runtime_.jsx("path", {
+                                                d: "M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"
+                                            })
+                                        ]
+                                    })
+                                })
+                            ]
+                        })
+                    ]
+                })
+            })
+        ]
+    });
+}
+
+;// CONCATENATED MODULE: ./components/Navbar.js
+
+
+
+function Navbar() {
+    return /*#__PURE__*/ jsx_runtime_.jsx("header", {
+        className: "text-gray-600 body-font bg-cyan-50",
+        children: /*#__PURE__*/ (0,jsx_runtime_.jsxs)("div", {
+            className: "container mx-auto flex py-4 flex-row items-center justify-center md:w-5/6 w-11/12",
+            children: [
+                /*#__PURE__*/ (0,jsx_runtime_.jsxs)("a", {
+                    className: "flex title-font font-medium items-center text-gray-900",
+                    children: [
+                        /*#__PURE__*/ jsx_runtime_.jsx((image_default()), {
+                            className: "w-12",
+                            src: "/logo.png",
+                            alt: "satria bimbel",
+                            width: 100,
+                            height: 100,
+                            priority: true
+                        }),
+                        /*#__PURE__*/ (0,jsx_runtime_.jsxs)("span", {
+                            className: "ml-3 text-xl font-bold",
+                            children: [
+                                "Satria ",
+                                /*#__PURE__*/ jsx_runtime_.jsx("span", {
+                                    className: " text-blue-300 ",
+                                    children: "Bimbel"
+                                })
+                            ]
+                        })
+                    ]
+                }),
+                /*#__PURE__*/ jsx_runtime_.jsx("nav", {
+                    className: "mr-auto ml-4 md:py-1 md:pl-4 md:border-gray-400 flex flex-wrap items-center text-base justify-center"
+                }),
+                /*#__PURE__*/ jsx_runtime_.jsx("a", {
+                    href: "https://api.whatsapp.com/send?phone=085243332776",
+                    target: "__blank",
+                    children: /*#__PURE__*/ jsx_runtime_.jsx("button", {
+                        className: "inline-flex items-center bg-green-500 border-0 px-3 py-2 focus:outline-none hover:bg-blue-300 rounded text-base text-white font-bold",
+                        children: "Kontak Kami"
+                    })
+                })
+            ]
+        })
+    });
+}
+
+// EXTERNAL MODULE: ./styles/globals.css
+var globals = __webpack_require__(764);
+;// CONCATENATED MODULE: external "next/head"
+const head_namespaceObject = require("next/head");
+var head_default = /*#__PURE__*/__webpack_require__.n(head_namespaceObject);
+;// CONCATENATED MODULE: ./pages/_app.js
+
+
+
+
+
+function App({ Component , pageProps  }) {
+    return /*#__PURE__*/ (0,jsx_runtime_.jsxs)(jsx_runtime_.Fragment, {
+        children: [
+            /*#__PURE__*/ (0,jsx_runtime_.jsxs)((head_default()), {
+                children: [
+                    /*#__PURE__*/ jsx_runtime_.jsx("link", {
+                        rel: "icon",
+                        href: "/app/favicon.ico"
+                    }),
+                    /*#__PURE__*/ jsx_runtime_.jsx("meta", {
+                        name: "viewport",
+                        content: "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+                    })
+                ]
+            }),
+            /*#__PURE__*/ jsx_runtime_.jsx(Navbar, {}),
+            /*#__PURE__*/ jsx_runtime_.jsx(Component, {
+                ...pageProps
+            }),
+            /*#__PURE__*/ jsx_runtime_.jsx(Footer, {})
+        ]
+    });
+}
+
+
+/***/ }),
+
+/***/ 764:
+/***/ (() => {
+
+
+
+/***/ }),
+
+/***/ 918:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/amp-context.js");
+
+/***/ }),
+
+/***/ 732:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/amp-mode.js");
+
+/***/ }),
+
+/***/ 796:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/head-manager-context.js");
+
+/***/ }),
+
+/***/ 486:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/image-blur-svg.js");
+
+/***/ }),
+
+/***/ 744:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/image-config-context.js");
+
+/***/ }),
+
+/***/ 843:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/image-config.js");
+
+/***/ }),
+
+/***/ 552:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/image-loader");
+
+/***/ }),
+
+/***/ 470:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/side-effect.js");
+
+/***/ }),
+
+/***/ 618:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/utils/warn-once.js");
+
+/***/ }),
+
+/***/ 689:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("react");
+
+/***/ }),
+
+/***/ 997:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("react/jsx-runtime");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = __webpack_require__.X(0, [636,675], () => (__webpack_exec__(74)));
+module.exports = __webpack_exports__;
+
+})();
